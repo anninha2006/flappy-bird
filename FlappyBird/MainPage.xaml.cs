f659b40b4c01f9d93c709c678aa452d6c3f8d7e5
anna <annaheloisa1809@gmail.com>
2024-10-10 11:51:29 -0300

    dfgdfh

---
+++ FlappyBird/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace FlappyBird;
+
+public partial class MainPage : ContentPage
+{
+    const int Gravidade = 1;
+	const int tempoEntreFrames = 25;
+	bool EstaMorto = false;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    void AplicaGravidade()
+	{
+		galinha.TranslationY+= Gravidade;
+	}
+	
+	async Task Desenha()
+	{
+	while (!EstaMorto)
+	{
+		AplicaGravidade();
+		await Task.Delay(tempoEntreFrames);
+	}
+	}
+	void OnGameOverCliked(object s, TappedEventArgs a)
+	{
+		frameGameOver.IsVisible = false;
+		Inicializar();
+		Desenha();
+	}
+	void Inicializar()
+	{
+		galinha.TranslationY = 0;
+		EstaMorto=false;
+	}
+	
+}
+
