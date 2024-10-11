1c1dfcf1833160f8cbeb62400d8163041a97b835
anna <annaheloisa1809@gmail.com>
2024-10-11 08:55:47 -0300

    feliz

--- FlappyBird/MainPage.xaml.cs
+++ FlappyBird/MainPage.xaml.cs
@@ -5,6 +5,9 @@ public partial class MainPage : ContentPage
     const int Gravidade = 1;
 	const int tempoEntreFrames = 25;
 	bool EstaMorto = false;
+	double largura_Janela = 0;
+	double altura_Janela = 0;
+	int velocidade = 20;
 	public MainPage()
 	{
 		InitializeComponent();
@@ -20,6 +23,7 @@ public partial class MainPage : ContentPage
 	{
 		AplicaGravidade();
 		await Task.Delay(tempoEntreFrames);
+		GerenciaTronco();
 	}
 	}
 	void OnGameOverCliked(object s, TappedEventArgs a)
@@ -33,6 +37,22 @@ public partial class MainPage : ContentPage
 		galinha.TranslationY = 0;
 		EstaMorto=false;
 	}
-	
+    protected override void OnSizeAllocated(double w, double h)
+    {
+        base.OnSizeAllocated(w, h);
+		largura_Janela = w;
+        altura_Janela = h;
+    }
+	void GerenciaTronco()
+	{
+     TroncoCima.TranslationX -= velocidade;
+	 TroncoBaixo.TranslationX -= velocidade;
+	 if (TroncoBaixo.TranslationX <-largura_Janela)
+	 {
+		TroncoBaixo.TranslationX = 0;
+		TroncoCima.TranslationX = 0;
+	 }
+	}
+
 }
 
